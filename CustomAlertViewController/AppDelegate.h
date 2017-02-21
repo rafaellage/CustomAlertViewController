7fadec59febd037f7d57c7f077446276b9ee8380
Eduardo Sanches Bocato <dubocato@gmail.com>
2017-02-21 19:57:33 -0300

    Initial Commit

---
+++ CustomAlertViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomAlertViewController
+//
+//  Created by Eduardo Sanches Bocato on 21/02/17.
+//  Copyright © 2017 bocato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomAlertViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertViewController
+//
+//  Created by Eduardo Sanches Bocato on 21/02/17.
+//  Copyright © 2017 bocato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
